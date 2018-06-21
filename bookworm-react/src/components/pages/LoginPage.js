7d181fa0ac078fbea6b11eedb4243384ac687734
Danial Munsif <danial.munsif@techno-soft.com>
2018-06-21 16:34:20 +0500

    unf

--- bookworm-react/src/components/pages/LoginPage.js
+++ bookworm-react/src/components/pages/LoginPage.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import connect from 'react-redux'
 import LoginForm from '../forms/LoginForm';
+import Login from '../../actions/auth';
 
 class LoginPage extends React.Component {
-    submit = (data) => {
-        console.log(data);
-    }
+    // login is the redux thunk method and history is passed as props becoz this is a route component
+    submit = (data) => this.props.login(data).then(() => this.props.history.push("/"))
     render() {
-        return (
-            <div>
-                <h1>Login Page</h1>
-                <LoginForm submit={this.submit} />
+        return ( <div>
+            <h1> Login Page </h1>
+            <LoginForm submit = { this.submit } />
             </div>
         );
     }
 }
+LoginPage.propTypes = {
+    login: PropTypes.fun.isRequired,
+    history: PropTypes.shape({
+        push: PropTypes.fun.isRequired
+    })
+}
+// first parameter allows us to pass redux state into this component as props
 
-export default LoginPage;
+export default connect(null, { login })(LoginPage);
